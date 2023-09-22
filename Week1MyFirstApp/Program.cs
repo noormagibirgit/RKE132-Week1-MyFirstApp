fd41cb7c75a04b5712cd6484b91896cba98a0207
Birgit <birgit.noormagi@tktk.ee>
2023-09-22 13:39:15 +0300

    add week1myfirstapp code

---
+++ Week1MyFirstApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// Tihs is a comment to my code 
+// rakendus küsib kasutaja nime
+//rakendus tervitab kasutajat nimepidi 
+
+
+Console.WriteLine("Enter your name "); // output
+
+string userName;
+
+userName = Console.ReadLine();  // input
+
+
+
+Console.WriteLine("Hello" + userName);  // output
+
+
+// string interpolation 
+
+Console.WriteLine($"Hello, {userName}!");  //output
+
